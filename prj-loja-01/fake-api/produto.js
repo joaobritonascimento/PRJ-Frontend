c0273f06208b1c4818d8c1a7fbfe5cc77cfa7a4a
joaobritonascimento <joaobritonascimento@gmail.com>
2023-01-24 02:10:22 +0000

    #02 - API Fake com arquivo js

--- prj-loja-01/fake-api/produto.js
+++ prj-loja-01/fake-api/produto.js
@@ -2,23 +2,14 @@
 //Objetos JSON dentro de um Array
 
 [
-    {"nome":"","img":"/home/ec2-user/PRJ-Frontend/prj-loja-01/fake-api/imagens/capacitor_ceramico.webp","valor":1.20},
-    {"nome":"","img":"/home/ec2-user/PRJ-Frontend/prj-loja-01/fake-api/imagens/ci_555.webp","valor":7.00},
-    {"nome":"","img":"/home/ec2-user/PRJ-Frontend/prj-loja-01/fake-api/imagens/diodo.webp","valor":2.80},
-    {"nome":"","img":"/home/ec2-user/PRJ-Frontend/prj-loja-01/fake-api/imagens/Ferro_de_solda.webp","valor":45.0},
-    {"nome":"","img":"/home/ec2-user/PRJ-Frontend/prj-loja-01/fake-api/imagens/Multimero_digital.webp","valor":90.0},
-    {"nome":"","img":"/home/ec2-user/PRJ-Frontend/prj-loja-01/fake-api/imagens/capacitor.webp","valor":1.30},
-    {"nome":"","img":"/home/ec2-user/PRJ-Frontend/prj-loja-01/fake-api/imagens/Diodo_zener.webp","valor":2.10},
-    {"nome":"","img":"/home/ec2-user/PRJ-Frontend/prj-loja-01/fake-api/imagens/Multimero_analogico.webp","valor":60.0},
-    {"nome":"","img":"/home/ec2-user/PRJ-Frontend/prj-loja-01/fake-api/imagens/resistor.webp","valor":0.80}
+    {"nome":"Capacitor cerâmico","img":"https://joaobritonascimento.github.io/PRJ-Frontend/prj-loja-01/fake-api/imagens/capacitor_ceramico.webp","valor":1.20},
+    {"nome":"CI 555","img":"https://joaobritonascimento.github.io/PRJ-Frontend/prj-loja-01/fake-api/imagens/ci_555.webp","valor":7.00},
+    {"nome":"Diodo","img":"https://joaobritonascimento.github.io/PRJ-Frontend/prj-loja-01/fake-api/imagens/diodo.webp","valor":2.80},
+    {"nome":"Ferro de solda","img":"https://joaobritonascimento.github.io/PRJ-Frontend/prj-loja-01/fake-api/imagens/Ferro_de_solda.webp","valor":45.0},
+    {"nome":"Multimetro Digital","img":"https://joaobritonascimento.github.io/PRJ-Frontend/prj-loja-01/fake-api/imagens/Multimero_digital.webp","valor":90.0},
+    {"nome":"Capacitor","img":"https://joaobritonascimento.github.io/PRJ-Frontend/prj-loja-01/fake-api/imagens/capacitor.webp","valor":1.30},
+    {"nome":"Diodo Zener","img":"https://joaobritonascimento.github.io/PRJ-Frontend/prj-loja-01/fake-api/imagens/Diodo_zener.webp","valor":2.10},
+    {"nome":"Multimento Analógico","img":"https://joaobritonascimento.github.io/PRJ-Frontend/prj-loja-01/fake-api/imagens/Multimero_analogico.webp","valor":60.0},
+    {"nome":"Resistor","img":"https://joaobritonascimento.github.io/PRJ-Frontend/prj-loja-01/fake-api/imagens/resistor.webp","valor":0.80}
 ]
 
-capacitor_ceramico.webp  
-ci_555.webp  
-diodo.webp        
-Ferro_de_solda.webp       
-Multimero_digital.webp
-capacitor.webp           
-Diodo_zener.webp  
-Multimero_analogico.webp  
-resistor.webp
